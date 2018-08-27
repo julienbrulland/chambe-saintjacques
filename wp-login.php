ca29bb8fc17d26f6934ec440f81e1c0645a94c0a
Julien <julien.brulland@gmail.com>
2018-08-27 16:06:14 +0200

    back-up

---
+++ wp-login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="fr-FR">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="fr-FR">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Se connecter &lsaquo; De Chambé à Saint-Jacques en 90 jours&#8230; &#8212; WordPress</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='./wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.9.8' type='text/css' media='all' />
+		<style type="text/css">
+		#wp_native_dashboard_language {padding: 2px;border-width: 1px;border-style: solid;height: 2em;vertical-align:top;margin-top: 2px;font-size:16px;width:100%; }
+		#wp_native_dashboard_language option { padding-left: 4px; }
+		</style>
+		<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+		</head>
+	<body class="login login-action-login wp-core-ui  locale-fr-fr">
+		<div id="login">
+		<h1><a href="https://wpfr.net/" title="Propulsé par WordPress" tabindex="-1">Propulsé par WordPress</a></h1>
+	
+<form name="loginform" id="loginform" action="./" method="post">
+	<p>
+		<label for="user_login">Nom d’utilisateur ou adresse e-mail<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Mot de passe<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+			<label>Language</label><br/>
+		<select id="wp_native_dashboard_language" name="wp_native_dashboard_language" tabindex="30">
+		<option value="en_US"><b>English</b></option><option value="fr_FR" selected="selected"><b>Français</b></option>		</select>
+		<br/><br/>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Se souvenir de moi</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Se connecter" />
+		<input type="hidden" name="redirect_to" value="./wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="./?action=lostpassword">Mot de passe oublié ?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="./index.html">&larr; Retour vers De Chambé à Saint-Jacques en 90 jours&#8230;</a></p>
+		
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
